a0b2bcb91c58d6329f65314ec91f878089502d80
Graber <graber29@purdue.edu>
2025-02-10 21:49:52 -0500

    small change to header file

--- PlatformIO-GuitarTuner/src/oled.c
+++ PlatformIO-GuitarTuner/src/oled.c
@@ -2,9 +2,6 @@
 // oled.c: Adapted from 362's "lcd.c" helper function as well as...
 //===========================================================================
 
-#include "stm32f407xx.h"
-#include <stdio.h>
-#include <stdint.h>
 #include "oled.h"
 
 void nano_wait(int t); // FROM ECE362 LABS
@@ -166,9 +163,9 @@ void OLED_Init(void (*reset)(int), void (*select)(int), void(*reg_select)(int))
     // OLED_WR_REG(0xF1); // NOT SURE ABOUT THIS
     OLED_WriteRegOnce(0xCA, 0x7F); // MUXRATIO - set MUX ratio to default of 127
     OLED_WriteRegOnce(0xA0, 0x74); // SETREMAP - swaps color sequence (check this), scan dir, enable COM split
-    uint8_t dataRowCol[] = {0x00, 0x7F}; // check this \/
-    OLED_WriteRegMult(0x15, &dataRowCol, sizeof(dataRowCol)); // SETCOL - sets to start of 0, end of 127
-    OLED_WriteRegMult(0x75, &dataRowCol, sizeof(dataRowCol)); // SETROW - sim ^, set strt 0, end 127
+    uint16_t dataRowCol[] = {0x00, 0x7F}; // check this \/
+    OLED_WriteRegMult(0x15, dataRowCol, sizeof(dataRowCol)); // SETCOL - sets to start of 0, end of 127
+    OLED_WriteRegMult(0x75, dataRowCol, sizeof(dataRowCol)); // SETROW - sim ^, set strt 0, end 127
     OLED_WriteRegOnce(0xA1, 0x00); // STARTLINE - set to default value of 0
     OLED_WriteRegOnce(0xA2, 0x00); // DISPLAYOFFSET - set to 0
     OLED_WriteRegOnce(0xB5, 0x00); // SETGPIO - set GP0 and 1 to HiZ and input disabled
@@ -176,11 +173,11 @@ void OLED_Init(void (*reset)(int), void (*select)(int), void(*reg_select)(int))
     OLED_WriteRegOnce(0xB1, 0x32); // PRECHARGE -  phase 1 period of 5 DCLKS, phase 2 period of 3 DCLKS
     OLED_WriteRegOnce(0xBE, 0x05); // VCOMH - set to default of 0.82xVcc for COM deselt voltage
     OLED_WR_REG(0xA6); // DISPLAYMODE - set to normal display mode :D 
-    uint8_t dataContrast[] = {0xC8, 0x80, 0xC8};
-    OLED_WriteRegMult(0xC1, &dataContrast, sizeof(dataContrast)); // SETCONTRAST - sets the contrast of each color value
+    uint16_t dataContrast[] = {0xC8, 0x80, 0xC8};
+    OLED_WriteRegMult(0xC1, dataContrast, sizeof(dataContrast)); // SETCONTRAST - sets the contrast of each color value
     OLED_WriteRegOnce(0xC7, 0x0F); // MASTERCONTRAST - default value
-    uint8_t dataSet[] = {0xA0, 0xB5, 0x55};
-    OLED_WriteRegMult(0xB4, &dataSet, sizeof(dataSet)); // SETSVL - External VSL, reset hard coded from manual
+    uint16_t dataSet[] = {0xA0, 0xB5, 0x55};
+    OLED_WriteRegMult(0xB4, dataSet, sizeof(dataSet)); // SETSVL - External VSL, reset hard coded from manual
     OLED_WriteRegOnce(0xB6, 0x01); // PRECHARGE2 - set precharge period to to 1 DCLKS
     OLED_WR_REG(0xAF); // SETSLEEPMODE - Set sleep mode OFF, display ON 
 
