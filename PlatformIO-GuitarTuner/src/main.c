4bbf4c2a31b3332381f4cba2f6af421540d52ca2
SG295 <sethg295@gmail.com>
2025-04-16 11:23:45 -0400

    Huge UI update

--- PlatformIO-GuitarTuner/src/main.c
+++ PlatformIO-GuitarTuner/src/main.c
@@ -13,9 +13,11 @@ void nano_wait(int t); // FROM ECE362 LABS
 // const char *R = "Right Button Pressed";
 // const char *M = "Middle Button Pressed";
 // const char *L = "Left Button Pressed";
-const char *R = "Full Capacity:     "; // "(mAh): "; 
-const char *M = "Remaining Capacity:"; // (mAh): ";
-const char *L = "Remaining Capacity:"; // (Perc): ";
+const char *R = "Right"; // "(mAh): "; 
+const char *M = "Middle"; // (mAh): ";
+const char *L = "Left"; // (Perc): ";
+const char *Tp = "Top"; // (Perc): ";
+const char *Bt = "Bottom"; // (Perc): ";
 const char *p = "HELD";
 
 #ifdef BATT_TESTING
@@ -26,9 +28,19 @@ const u8 batt_comms[3] = { // for battery prototyping
     BQ27441_COMMAND_SOC
 };
 
+char *directions[] = {"0", "1"};
+u8 direct = 0; 
+
 u8 index_batt = 0; 
 
 #endif
+
+u8 buttons = 0; // 8 bit 'register' to hold flags for each buttons at a certain bit
+/*
+button 1 ^= 1 << 0;
+button 2 ^= 1 << 1; 
+and so on... 
+*/
 
 uint8_t held = 0;
 
@@ -42,10 +54,11 @@ typedef enum {
     MAIN_MENU,
     FREE_SPIN,
     DIGITAL_TUNER,
-    BATTERY_CHECK
+    BATTERY_CHECK,
+    BOOT
 } state_t; 
 
-state_t state = MAIN_MENU; 
+state_t state = BOOT; 
 
 const uint8_t arrow_left_pos = 22;
 const uint8_t arrow_right_pos = 92;
@@ -56,6 +69,8 @@ char output_batt[20];
 int32_t sample;
 void i2c_send_address(uint8_t address);
 void i2c_read_address(uint8_t reads, char *data);
+
+void write_menu();
 
 const float standard_tuning[6] =
 {
@@ -88,17 +103,6 @@ void initd()
     // GPIOD -> BSRR = (1 << 12) | (1 << 13) | (1 << 14); // set high
 }
 
-void init_buttons()
-{
-    RCC -> AHB1ENR |= RCC_AHB1ENR_GPIOBEN; 
-
-    // Turn on PD7, 5, 3 and as INPUTS for buttons
-    GPIOB -> MODER &= ~0xCCC00000; // clear to ensure input
-    // GPIOB -> PUPDR &= ~0xCCC00000; 
-    // GPIOB -> PUPDR |= 0x88800000; // Pull down 
-
-}
-
 void togglexn(GPIO_TypeDef *port, int pos)
 {
     port -> ODR ^= (1 << pos);
@@ -120,7 +124,7 @@ void init_exti()
     SYSCFG -> EXTICR[1] |= 0x222; // Set 4-6 for bus C
 
     EXTI -> FTSR |= EXTI_FTSR_TR0 | EXTI_FTSR_TR1 | EXTI_FTSR_TR2 | EXTI_FTSR_TR4 | EXTI_FTSR_TR5 | EXTI_FTSR_TR6; // Set to falling edge trigger
-    EXTI -> IMR |= EXTI_IMR_MR1 | EXTI_IMR_MR4 | EXTI_IMR_MR5 | EXTI_IMR_MR6 | EXTI_IMR_MR0 | EXTI_IMR_MR2;; // Unmask interrupts in IMR
+    EXTI -> IMR |= EXTI_IMR_MR4 | EXTI_IMR_MR5 | EXTI_IMR_MR6 | EXTI_IMR_MR0 | EXTI_IMR_MR2; // Unmask interrupts in IMR - EXCEPT 1 WHICH IS TRIGGER
 
     NVIC -> ISER[0] |= (1<<EXTI0_IRQn) | (1<<EXTI1_IRQn) | (1<<EXTI2_IRQn) | (1<<EXTI4_IRQn) | (1<<EXTI9_5_IRQn); // Enable interrupts in vector table
 }
@@ -187,105 +191,281 @@ void TIM3_IRQHandler(void)
     if((GPIOC->IDR & (1 << 1)) == 0) // held, active low buttons
     {
         held = 1; 
-        OLED_DrawString(0, 24, WHITE, BLACK, p, 12);
-        /*
-        MOTOR CONTROLS HERE:
-        if(state == STANDARD_TUNING)
+        OLED_DrawString(0, 30, WHITE, BLACK, "*", 12);
+        if(state == FREE_SPIN)
         {
-            if(standard_tuning[tuning_i] == (mic_input))
-            {
-                //success, probably need some threshold
-
-                TIM3 -> CR1 &= ~TIM_CR1_CEN; // disable this timer
-            }
-            else
-            {
-                if(standard_tuning[tuning_i] > mic_input)
-                {
-                    drive_motor();
-                }
-                else
-                {
-                    drive_motor(); 
-                }
-            }
+            drive_motor(200);
         }
-        else if(state == FREE_SPIN)
+        else
         {
-
+            // Logic for standard tuning states here!
         }
-        */
     }
     else 
     {
         TIM3 -> CR1 &= ~TIM_CR1_CEN; // disable timer
         held = 0; // reset held
-        OLED_Clear(BLACK); 
+        OLED_DrawString(0, 30, WHITE, BLACK, "  ", 12);
     }
+}
+
+void init_tim4()
+{
+    RCC -> APB1ENR |= RCC_APB1ENR_TIM4EN; //enable clock 
+
+    TIM4 -> PSC = 16800-1;
+    TIM4 -> ARR = 250-1; 
+    TIM4 -> DIER |= TIM_DIER_UIE; 
+    TIM4 -> CR1 |= TIM_CR1_ARPE; 
+    NVIC_EnableIRQ(TIM4_IRQn);
+}
+
+void TIM4_IRQHandler(void)
+{
+    TIM4 -> SR &= ~TIM_SR_UIF;
+    switch(state)
+    {
+        case STANDARD_TUNING_1: // E
+            if(buttons & (1 << 0)) // right button press
+            {
+                state = STANDARD_TUNING_2;
+                OLED_DrawArrow(arrow_left_pos, 57, BLACK, 0);
+                OLED_DrawArrow(arrow_left_pos, 57+(17), B_Color, 0);
+                buttons &= ~(1<<0);
+            }
+            else if(buttons & (1 << 2)) // left button press
+            {   
+                state = MAIN_MENU;
+                write_menu();
+                buttons &= ~(1<<2);
+            }
+            break;
+        case STANDARD_TUNING_2: // A
+            if(buttons & (1 << 0)) // right button press
+            {
+                state = STANDARD_TUNING_3;
+                OLED_DrawArrow(arrow_left_pos, 57+(17), BLACK, 0);
+                OLED_DrawArrow(arrow_left_pos, 57+(17*2), B_Color, 0);
+                buttons &= ~(1<<0);
+            }
+            else if(buttons & (1 << 2)) // left button press
+            {   
+                state = STANDARD_TUNING_1;
+                OLED_DrawArrow(arrow_left_pos, 57+(17), BLACK, 0);
+                OLED_DrawArrow(arrow_left_pos, 57, B_Color, 0);
+                buttons &= ~(1<<2);
+            }
+            break;
+        case STANDARD_TUNING_3: // D
+            if(buttons & (1 << 0)) // right button press
+            {
+                state = STANDARD_TUNING_4;
+                OLED_DrawArrow(arrow_right_pos, 57, B_Color, 1);
+                OLED_DrawArrow(arrow_left_pos, 57+(17*2), BLACK, 0);
+                buttons &= ~(1<<0);
+            }
+            else if(buttons & (1 << 2)) // left button press
+            {   
+                state = STANDARD_TUNING_2;
+                OLED_DrawArrow(arrow_left_pos, 57+(17*2), BLACK, 0);
+                OLED_DrawArrow(arrow_left_pos, 57+(17), B_Color, 0);
+                buttons &= ~(1<<2);
+            }
+            break;
+        case STANDARD_TUNING_4: // G
+            if(buttons & (1 << 0)) // right button press
+            {
+                state = STANDARD_TUNING_5;
+                OLED_DrawArrow(arrow_right_pos, 57+(17), B_Color, 1);
+                OLED_DrawArrow(arrow_right_pos, 57, BLACK, 1);
+                buttons &= ~(1<<0);
+            }
+            else if(buttons & (1 << 2)) // left button press
+            {   
+                state = STANDARD_TUNING_3;
+                OLED_DrawArrow(arrow_right_pos, 57, BLACK, 1);
+                OLED_DrawArrow(arrow_left_pos, 57+(17*2), B_Color, 0);
+                buttons &= ~(1<<2);
+            }
+            break;
+        case STANDARD_TUNING_5: // B
+            if(buttons & (1 << 0)) // right button press
+            {
+                state = STANDARD_TUNING_6;
+                OLED_DrawArrow(arrow_right_pos, 57+(17*2), B_Color, 1);
+                OLED_DrawArrow(arrow_right_pos, 57+(17), BLACK, 1);
+                buttons &= ~(1<<0);
+            }
+            else if(buttons & (1 << 2)) // left button press
+            {   
+                state = STANDARD_TUNING_4;
+                OLED_DrawArrow(arrow_right_pos, 57, B_Color, 1);
+                OLED_DrawArrow(arrow_right_pos, 57+(17), BLACK, 1);
+                buttons &= ~(1<<2);
+            }
+            break;
+        case STANDARD_TUNING_6: // E
+            if(buttons & (1 << 0)) // right button press
+            {
+                state = MAIN_MENU;
+                write_menu();
+                buttons &= ~(1<<0);
+            }
+            else if(buttons & (1 << 2)) // left button press
+            {   
+                state = STANDARD_TUNING_5;
+                OLED_DrawArrow(arrow_right_pos, 57+(17), B_Color, 1);
+                OLED_DrawArrow(arrow_right_pos, 57+(17*2), BLACK, 1);
+                buttons &= ~(1<<2);
+            }
+            break;
+        case MAIN_MENU:
+            if(buttons & (1 << 0)) // right button press
+            {   
+                state = STANDARD_TUNING_1;
+                OLED_DrawString(16, 0, WHITE, BLACK, "             ", 16);
+                OLED_DrawString(12, 0, WHITE, BLACK, "Hold Trigger  ", 16);
+                OLED_DrawString(0, 16, WHITE, BLACK, "to allow tuning", 16);
+                OLED_DrawGuitar();
+                OLED_DrawString(0, 111, WHITE, BLACK, "<", 16);
+                OLED_DrawString(119, 111, WHITE, BLACK, ">", 16);
+                OLED_DrawArrow(arrow_left_pos, 57, B_Color, 0);
+                buttons &= ~(1<<0);
+            }
+            else if(buttons & (1 << 2)) // left button press
+            {   
+                state = BATTERY_CHECK;
+                OLED_Clear(BLACK);
+                OLED_DrawString(0, 63, WHITE, BLACK, "Battery Chk", 12);    
+                buttons &= ~(1<<2);
+            }
+            else if(buttons & (1 << 4)) // top button press
+            {   
+                state = FREE_SPIN;
+                OLED_Clear(BLACK);
+                OLED_DrawString(26, 0, B_Color, BLACK, "Free Spin", 16);   
+                OLED_DrawString(20, 16, WHITE, BLACK, "Press right to", 12);
+                OLED_DrawString(12, 28, WHITE, BLACK, "toggle direction", 12); 
+                OLED_DrawString(0, 56, A_Color, BLACK, "Direction:", 16); 
+                OLED_DrawString(82, 56, A_Color, BLACK, "0", 16);   
+                buttons &= ~(1<<4);
+            }
+            else if(buttons & (1 << 5)) // down button press
+            {   
+                state = DIGITAL_TUNER;
+                OLED_Clear(BLACK);
+                OLED_DrawString(0, 63, WHITE, BLACK, "Digital Tuner", 12);    
+                buttons &= ~(1<<5);
+            }
+            break;
+        case FREE_SPIN:
+            if(buttons & (1 << 5)) // down button press
+            {   
+                state = MAIN_MENU;
+                write_menu();  
+                buttons &= ~(1<<5);
+            }
+            else if(buttons & (1 << 0)) // right button press
+            {   
+                direct ^= 1; // toggle direction 
+                OLED_DrawString(82, 56, A_Color, BLACK, directions[direct], 16);  
+            }
+            break;
+        case BATTERY_CHECK:
+            if(buttons & (1 << 0)) // right button press
+            {   
+                state = MAIN_MENU;
+                write_menu();
+                buttons &= ~(1<<0);
+            }
+            break;
+        case DIGITAL_TUNER:
+            if(buttons & (1 << 4)) // top button press
+            {   
+                state = MAIN_MENU;
+                write_menu();    
+                buttons &= ~(1<<4);
+            }
+            break; 
+        default:
+            state = MAIN_MENU;
+            write_menu();
+            buttons = 0; // clear all buttons, shouldn't get here
+    }
+    // OLED_Clear(BLACK);
+    // OLED_DrawString(0, 63, WHITE, BLACK, Tp, 12);
+    if((state == FREE_SPIN) || (state == STANDARD_TUNING_1) || (state == STANDARD_TUNING_2) || (state == STANDARD_TUNING_3) || (state == STANDARD_TUNING_4) || (state == STANDARD_TUNING_5) || (state == STANDARD_TUNING_6))
+    {
+        EXTI -> IMR |= EXTI_IMR_MR1; // if state allows motor spin, turn on that EXTI
+    }
+    else
+    {
+        EXTI -> IMR &= ~EXTI_IMR_MR1; // otherwise, ENSURE it's off
+    }
+    TIM4 -> CR1 &= ~TIM_CR1_CEN;
+    // EXTI -> IMR |= (EXTI_IMR_MR4);
 }
 
 void EXTI0_IRQHandler() // Full capacity
 {
     
     EXTI -> PR |= EXTI_PR_PR0; // Clear pending bit
+    buttons |= 1 << 0;
     // GPIOD -> BSRR = (1 << 12);
-    OLED_Clear(BLACK);
-    OLED_DrawString(0, 63, WHITE, BLACK, R, 12);
-    // GPIOD -> BSRR = (1 << 12) << 16;
-    i2c_send_address(BQ27441_COMMAND_FULL_CAP_FIL);// BQ27441_COMMAND_REM_CAPACITY);
-    i2c_read_address(2, data_c);
-    data_buffer = (data_c[1] << 8) | data_c[0];
-    sprintf(output_batt, "%d", data_buffer);
-    OLED_DrawString(0, 80, WHITE, BLACK, output_batt, 12);
-    OLED_DrawString(28, 80, WHITE, BLACK, "mAh", 12);
+    // OLED_Clear(BLACK);
+    // OLED_DrawString(0, 63, WHITE, BLACK, R, 12);
+    TIM4 -> CR1 |= TIM_CR1_CEN;
 }
 
 void EXTI1_IRQHandler() // Remaining capacity 
 {
     EXTI -> PR |= EXTI_PR_PR1; // Clear pending bit
-    OLED_Clear(BLACK);
-    OLED_DrawString(0, 63, WHITE, BLACK, M, 12);
+    buttons |= 1 << 1;
+    // OLED_Clear(BLACK);
+    // OLED_DrawString(0, 63, WHITE, BLACK, M, 12);
     // EXTI -> IMR |= EXTI_IMR_MR0 | EXTI_IMR_MR2;
     // TIM3 -> CNT = 0; //reset count to 0 
     // TIM3 -> CR1 |= TIM_CR1_CEN; //enable
-    i2c_send_address(BQ27441_COMMAND_REM_CAPACITY);
-    i2c_read_address(2, data_c);
-    data_buffer = (data_c[1] << 8) | data_c[0];
-    sprintf(output_batt, "%d", data_buffer);
-    OLED_DrawString(0, 80, WHITE, BLACK, output_batt, 12);
-    OLED_DrawString(24, 80, WHITE, BLACK, "mAh", 12);
+    TIM3 -> CR1 |= TIM_CR1_CEN;
 }
 
 void EXTI2_IRQHandler() // State of charge 
 {  
-    data_buffer = 0;
     EXTI -> PR |= EXTI_PR_PR2; // Clear pending bit
+    buttons |= 1 << 2;
     // GPIOD -> BSRR = (1 << 14);
-    OLED_Clear(BLACK);
-    OLED_DrawString(0, 63, WHITE, BLACK, L, 12);
-    // GPIOD -> BSRR = (1 << 14) << 16;
-    i2c_send_address(BQ27441_COMMAND_SOC);// BQ27441_COMMAND_REM_CAPACITY);
-    i2c_read_address(2, data_c);
-    data_buffer = (data_c[1] << 8) | data_c[0];
-    sprintf(output_batt, "%d", data_buffer);
-    OLED_DrawString(0, 80, WHITE, BLACK, output_batt, 12);
-    OLED_DrawString(24, 80, WHITE, BLACK, "%", 12);
+    // OLED_Clear(BLACK);
+    // OLED_DrawString(0, 63, WHITE, BLACK, L, 12);
+    TIM4 -> CR1 |= TIM_CR1_CEN;
 }
 
 void EXTI4_IRQHandler()
 {
     EXTI -> PR |= EXTI_PR_PR4; // Clear pending bit
+    buttons |= 1 << 4;
+    // OLED_Clear(BLACK);
+    // OLED_DrawString(0, 63, WHITE, BLACK, Tp, 12);
+    // EXTI -> IMR &= ~(EXTI_IMR_MR4); 
+    TIM4 -> CR1 |= TIM_CR1_CEN;
 }
 
 void EXTI9_5_IRQHandler()
 {
-    if(EXTI -> PR & EXTI_PR_PR5_Pos)
+    if(EXTI -> PR & EXTI_PR_PR5)
     {
         EXTI -> PR |= EXTI_PR_PR5; // Clear pending bit for 5
+        buttons |= 1 << 5;
+        // OLED_Clear(BLACK);
+        // OLED_DrawString(0, 63, WHITE, BLACK, Bt, 12);
+        TIM4 -> CR1 |= TIM_CR1_CEN;
     }
-    else if(EXTI -> PR & EXTI_PR_PR6_Pos)
+    else if(EXTI -> PR & EXTI_PR_PR6)
     {
         EXTI -> PR |= EXTI_PR_PR6; // Clear pending bit for 6
+        buttons |= 1 << 6;
+        // OLED_Clear(BLACK);
+        // OLED_DrawString(0, 63, WHITE, BLACK, L, 12);
+        TIM4 -> CR1 |= TIM_CR1_CEN;
     }
 }
 
@@ -366,39 +546,44 @@ void i2c_read_address(uint8_t reads, char *data) // should go right after a send
     }
 }
 
+void write_menu()
+{
+    OLED_Clear(BLACK);
+    OLED_DrawString(12, 0, WHITE, BLACK, "ECE477 Guitar", 16);
+    OLED_DrawString(44, 16, WHITE, BLACK, "Tuner", 16);
+    OLED_DrawString(20, 115, B_Color, BLACK, "\\/ Digi Tuner", 12);
+    OLED_DrawString(20, 95, B_Color, BLACK, "< Batt Stats", 12);
+    OLED_DrawString(20, 75, B_Color, BLACK, "> Auto Tune", 12);
+    OLED_DrawString(20, 55, B_Color, BLACK, "/\\ Free Spin", 12);
+    /*
+            ECE GUITAR
+               TUNER
+            /\ Free Spin
+    
+    < Batt Stats      Auto Tune >
+    
+            \/ Digi Tuner
+    */
+
+}
+
 int main(void)
 {
     init_spi1(); 
-    // initd();
-    // init_exti();
+    init_exti();
+    init_tim4(); 
     init_i2c_BQ27441(); 
-    // init_DRV();
-    // init_gpio_mic();
-    // clock_enable(); 
-    // init_i2s_mic();
+    init_DRV();
+    init_gpio_mic();
+    clock_enable(); 
+    init_i2s_mic();
+    init_tim3();
 
     OLED_Setup(); 
     OLED_Clear(BLACK); 
 
-    const char *S = "ECE477 Guitar";// Tuner";
-
-    const char *Z = "Tuner";
-
-    OLED_DrawString(16, 0, WHITE, BLACK, S, 16);
-    OLED_DrawString(48, 16, WHITE, BLACK, Z, 16);
-
-    init_tim2();
-    // const char *T = "Press middle button";
-
-    // OLED_DrawString(0, 52, WHITE, BLACK, T, 12);
-
-    // const char *H = "to begin standard";
-
-    // OLED_DrawString(0, 64, WHITE, BLACK, H, 12);
-
-    // const char *X = "tuning.";
-
-    // OLED_DrawString(0, 76, WHITE, BLACK, X, 16);
+    init_tim3();
+    init_tim4(); 
 
     // OLED_DrawGuitar();
 
@@ -406,11 +591,29 @@ int main(void)
 
     // OLED_DrawArrow(arrow_right_pos, 57+(17*2), B_Color, 1);
 
-    // drive_motor(100); 
+    // Check battery charge and prevent boot if too low:
+    // i2c_send_address(BQ27441_COMMAND_SOC); // get percent charge
+    // i2c_read_address(2, data_c);
+    // data_buffer = (data_c[1] << 8) | data_c[0];
+    data_buffer = 10; // hardcode for testing
+    if(data_buffer <= 3) // if percent charge is less than 3
+    {
+        OLED_DrawString(32, 50, C_Color, BLACK, "LOW BATT", 16);
+        OLED_DrawString(34, 66, WHITE, BLACK, "Power off", 12);
+        OLED_DrawString(34, 78, WHITE, BLACK, "and charge", 12);
+        while(1); // hold here until power off
+    }
+    else
+    {
+        state = MAIN_MENU; 
+        write_menu();
+    }
 
     for(;;)
     {
-        
+        // wait for interrupts \(^-^)/ 
     }
 }
-// PB6 (SCL) and PB7 (SDA) for Battery Management
+// Notes: 
+//  I2C: PB6 (SCL) and PB7 (SDA) for Battery Management
+//  SPI: CS - PA4, SCK - PA5, MISO - PA6, MOSI - PA7, DC - PA8, RST - PA9
